f6946e94bede061145ef0929fce0d16d49922cd2
tylerchessa <tylerchessa@gmail.com>
2023-02-14 21:31:03 +0000

    updated

--- typewriter.js
+++ typewriter.js
@@ -16,19 +16,3 @@ const typewriter = (inputString) => {
 
 typewriter(sentence)
 
-
-
-
-
-// const sentence = "hello there from lighthouse labs";
-// let time = 0;
-// for (const char of sentence) {
-//   setTimeout(() => {
-//     if (sentence[-1] === char) {
-//       console.log("sentence[-1]", sentence[-1]);
-//       process.stdout.write(`${char} \n`);
-//     }
-//     process.stdout.write(char);
-//   }, time);
-//   time += 50;
-// }
